ef37a87cb532234592640c25cd6d762893e5eaa6
joserivas7 <jose.julian18@gmail.com>
2022-12-04 23:04:59 -0300

    Entregablecompleto

--- Appclase/views.py
+++ Appclase/views.py
@@ -8,9 +8,17 @@ from django.template import loader
 
 def familiares (request):
 
-    fami=familia(nombre="Daniel", apellido="Rivas", parentesco="Hermano", edad=32)
+    fami=familia(nombre="Daniel", apellido="Rivas", parentesco="Hermano", edad=22)
+    fami2=familia(nombre="Jose", apellido="Rivas", parentesco="Primo", edad=32)
+    fami3=familia(nombre="David", apellido="Rivas", parentesco="Tio", edad=42)
     fami.save()
-    fami={"nombre":fami.nombre, "apellido":fami.apellido, "parentesco":fami.parentesco, "edad":fami.edad}
+    fami2.save()
+    fami3.save()
+
+    fami={"nombre":fami.nombre, "apellido":fami.apellido, "parentesco":fami.parentesco, "edad":fami.edad, 
+    "nombre2":fami2.nombre, "apellido2":fami2.apellido, "parentesco2":fami2.parentesco, "edad2":fami2.edad,
+    "nombre3":fami3.nombre, "apellido3":fami3.apellido, "parentesco3":fami3.parentesco, "edad3":fami3.edad
+    }
  
     template=loader.get_template("template.html")
 
